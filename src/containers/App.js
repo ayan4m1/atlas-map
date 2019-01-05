4d506cc2fe3f3bb43ea1a65b8936971dca73f941
ayan4m1 <andrew@bulletlogic.com>
2019-01-04 22:13:06 -0500

    remove bootstrap

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 
 export class App extends Component {
-  get heading() {
+  render() {
     return (
-      <nav className="navbar navbar-dark sticky-top bg-dark p-0">
-        <span className="navbar-brand col-sm-3 col-md-2 mr-0">
-          <span className="d-none d-sm-none d-md-inline pl-2">Atlas Map</span>
-        </span>
-      </nav>
-    );
-  }
-
-  get map() {
-    return (
-      <Map className="h-100" center={[0, 0]} zoom={1}>
+      <Map className="full-height" center={[0, 0]} zoom={2} maxZoom={6}>
         <TileLayer
           url="http://localhost:8080/base/{z}/{x}/{y}.png"
           attribution='<a href="http://playatlas.com">ATLAS</a> &copy; Grapeshot Games'
         />
       </Map>
-    );
-  }
-
-  render() {
-    return (
-      <div className="h-100">
-        {this.heading}
-        <main>{this.map}</main>
-      </div>
     );
   }
 }
