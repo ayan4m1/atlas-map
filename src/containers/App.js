ae42cbcd5f01917f0c6af0c691197c8438f54106
ayan4m1 <andrew@bulletlogic.com>
2019-01-05 11:20:49 -0500

    change tile image format from png to webp

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ export class App extends Component {
       <Map className="full-height" center={[0, 0]} zoom={2} maxZoom={6}>
         <TileLayer
           attribution='<a rel="noopener noreferrer" target="_blank" href="http://playatlas.com">ATLAS</a> &copy; Grapeshot Games</span>'
-          url={`${config.baseUrl}/{z}/{x}/{y}.png`}
+          url={`${config.baseUrl}/{z}/{x}/{y}.webp`}
         />
       </Map>
     );
